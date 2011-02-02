bef1df9aa9ae494862a12a009657ef24ffe9b1e0
Thomas Aylott <oblivious+git@subtlegradient.com>
2011-02-02 15:29:31 -0800

    Tweaked escape completions

--- Support/generateCompletionsArray.js
+++ Support/generateCompletionsArray.js
@@ -26,7 +26,9 @@ var fs = require('fs');
 
 require.paths.unshift('./lib');
 
-var completionArray = [];
+function push(it){ this[it] = true }
+
+var completions = {};
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 /*
@@ -38,30 +40,27 @@ var API = JSON.parse(fs.readFileSync(ROOT_DIR + '/api.json'));
 for (var namespace in API){
 	if (!hasOwnProperty.call(API, namespace)) continue;
 	
-	completionArray.push(namespace)
-	completionArray.push(namespace.replace('tanium',''))
+	push.call(completions, namespace)
+	push.call(completions, namespace.replace('tanium',''))
 	
 	for (var i = 0; i < API[namespace].methods.length; ++i){
-		completionArray.push(API[namespace].methods[i].name+'()')
-		completionArray.push(namespace + '.' + API[namespace].methods[i].name+'()')
-		completionArray.push(namespace.replace('tanium','') + '.' + API[namespace].methods[i].name+'()')
+		push.call(completions, API[namespace].methods[i].name)
+		// push.call(completions, namespace + '.' + API[namespace].methods[i].name)
+		// push.call(completions, namespace.replace('tanium','') + '.' + API[namespace].methods[i].name)
+		
+		push.call(completions, API[namespace].methods[i].name+'()')
+		push.call(completions, namespace + '.' + API[namespace].methods[i].name+'()')
+		push.call(completions, namespace.replace('tanium','') + '.' + API[namespace].methods[i].name+'()')
 	}
 	
 	for (var i = 0; i < API[namespace].properties.length; ++i){
-		completionArray.push(API[namespace].properties[i].name)
-		completionArray.push(namespace + '.' + API[namespace].properties[i].name)
-		completionArray.push(namespace.replace('tanium','') + '.' + API[namespace].properties[i].name)
+		push.call(completions, API[namespace].properties[i].name)
+		push.call(completions, namespace + '.' + API[namespace].properties[i].name)
+		push.call(completions, namespace.replace('tanium','') + '.' + API[namespace].properties[i].name)
 	}
 }
-sys.puts(sys.inspect(completionArray))
-// fs.writeFileSync(ROOT_DIR + '/completionArray.json', JSON.stringify(completionArray))
+sys.puts(( '<string>' + Object.keys(completions).sort() .join('</string>\n<string>') + '</string>' ))
+// fs.writeFileSync(ROOT_DIR + '/completions.json', JSON.stringify(completions))
 
-sys.print('Done!')
+// sys.print('Done!')
 
-// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-
-function addMissingStuff(){
-	// currentWindow
-	// currentTab
-	// currentTabGroup
-}
